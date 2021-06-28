3a7da0092951c8f86a649e214d0f2d36f1df4ffe
Christine Dodrill <me@christine.website>
2021-06-28 19:45:02 -0400

    initial commit
    Signed-off-by: Christine Dodrill <me@christine.website>

---
+++ app/src/main/java/website/christine/xesite/DelegationService.java
@@ -0,0 +1,18 @@
+package website.christine.xesite;
+
+
+import com.google.androidbrowserhelper.locationdelegation.LocationDelegationExtraCommandHandler;
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+            registerExtraCommandHandler(new LocationDelegationExtraCommandHandler());
+        
+    }
+}
+
